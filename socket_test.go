e928392720f1ff3cd637fd3fa819ba79f60112ba
RRvW <rrvanwijk+github@operamail.com>
2018-06-25 14:17:28 +0200

    Clean up tests

--- socket_test.go
+++ socket_test.go
@@ -5,12 +5,29 @@
 package canbus_test
 
 import (
+	"github.com/TUDelftSBT/canbus"
 	"log"
 	"reflect"
 	"testing"
-
-	"github.com/go-daq/canbus"
 )
+
+func setupSocket(t *testing.T, dev string) *canbus.Socket {
+	r, err := canbus.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = r.Bind(dev)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return r
+}
+
+func teardownSocket(t *testing.T, s *canbus.Socket) {
+	if err := s.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestSocket(t *testing.T) {
 	const (
@@ -20,62 +37,78 @@ func TestSocket(t *testing.T) {
 	)
 	var msg = []byte{0, 0xde, 0xad, 0xbe, 0xef}
 
-	r, err := canbus.New()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer r.Close()
+	r := setupSocket(t, endpoint)
+	w := setupSocket(t, endpoint)
 
-	w, err := canbus.New()
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer w.Close()
+	t.Run("Recv", func(t *testing.T) {
+		go func() {
+			for i := 0; i < N; i++ {
+				msg[0] = byte(i)
+				_, err := w.Send(ID, msg)
+				if err != nil {
+					t.Fatalf("error send[%d]: %v\n", i, err)
+				}
 
-	err = r.Bind(endpoint)
-	if err != nil {
-		log.Fatal(err)
-	}
+			}
+		}()
 
-	err = w.Bind(endpoint)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	go func() {
 		for i := 0; i < N; i++ {
-			msg[0] = byte(i)
-			_, err := w.Send(ID, msg)
+			id, data, err := r.Recv()
 			if err != nil {
-				t.Fatalf("error send[%d]: %v\n", i, err)
+				t.Fatalf("error recv: %v\n", err)
+			}
+			if id != ID {
+				t.Errorf("got id=%v. want=%d\n", id, ID)
+			}
+			if got, want := int(data[0]), i; got != want {
+				t.Errorf("error index: got=%d. want=%d\n", got, want)
+			}
+
+			if !reflect.DeepEqual(data[1:], msg[1:]) {
+				t.Errorf("error data:\ngot= %v\nwant=%v\n", data[1:], msg[1:])
+			}
+		}
+	})
+
+	t.Run("RecvRaw", func(t *testing.T) {
+		go func() {
+			for i := 0; i < N; i++ {
+				msg[0] = byte(i)
+				_, err := w.Send(ID, msg)
+				if err != nil {
+					t.Fatalf("error send[%d]: %v\n", i, err)
+				}
+
+			}
+		}()
+		canmsg := canbus.CANMessage{}
+
+		for i := 0; i < N; i++ {
+			err := r.RecvRaw(&canmsg)
+
+			id := canmsg.GetID()
+			can_dlc := canmsg.GetLen()
+			if err != nil {
+				t.Fatalf("error recv: %v\n", err)
+			}
+			if id != ID {
+				t.Errorf("got id=%v. want=%d\n", id, ID)
+			}
+
+			data := canmsg.GetData()
+			if got, want := int(data[0]), i; got != want {
+				t.Errorf("error index: got=%d. want=%d\n", got, want)
+			}
+
+			if !reflect.DeepEqual(data[1:can_dlc], msg[1:]) {
+				t.Errorf("error data:\ngot= %v\nwant=%v\n", data[1:], msg[1:])
 			}
 
 		}
-	}()
 
-	for i := 0; i < N; i++ {
-		id, data, err := r.Recv()
-		if err != nil {
-			t.Fatalf("error recv: %v\n", err)
-		}
-		if id != ID {
-			t.Errorf("got id=%v. want=%d\n", id, ID)
-		}
-		if got, want := int(data[0]), i; got != want {
-			t.Errorf("error index: got=%d. want=%d\n", got, want)
-		}
+	})
 
-		if !reflect.DeepEqual(data[1:], msg[1:]) {
-			t.Errorf("error data:\ngot= %v\nwant=%v\n", data[1:], msg[1:])
-		}
-	}
-
-	if err := w.Close(); err != nil {
-		t.Fatal(err)
-	}
-
-	if err := r.Close(); err != nil {
-		t.Fatal(err)
-	}
+	teardownSocket(t, r)
+	teardownSocket(t, w)
 
 }
